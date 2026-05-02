fcaa487d6a50906d314d6db943f43ac94db4b891
Brozi <jasiubar@gmail.com>
2026-05-03 01:53:30 +0200

    feat: add --version command

--- src/ilf/cli.py
+++ src/ilf/cli.py
@@ -3,7 +3,7 @@ from src.ilf import ExitCode, ERROR_MESSAGES
 from src.ilf import __app_name__, __version__
 from typing import Optional
 import typer
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 
 def _version_callback(value: bool):
     #--version command. value: bool passes the state of this command
@@ -11,18 +11,20 @@ def _version_callback(value: bool):
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
         raise typer.Exit()
+@app.callback()
 def main(
-        version: Optional[bool] = typer.Option(
-            False,
-            "--version",
-            "-v",
-            help=f"Show the {__app_name__}'s version and exit.",
-            callback=_version_callback,
-            is_eager=True,
-        )
+    _version: Optional[bool] = typer.Option(
+        False,
+        "--version",
+        "-v",
+        help=f"Show the {__app_name__}'s version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
 ) -> None:
     """
-    Inpost Locker Finder (ilf or locker finder for short): A tool for finding
+    inpost locker finder (ilf or locker finder for short): A tool for finding
     nearest inpost lockers.
+    For help type ilf <command> --help/-h
     """
     return
