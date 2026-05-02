be4ddb3280ff9facbf9b10da57efbd2a65a39d12
Brozi <jasiubar@gmail.com>
2026-05-02 22:51:52 +0200

    feat: add tests

---
+++ tests/test_api.py
@@ -0,0 +1,46 @@
+import pytest
+from unittest.mock import patch
+from src.ilf.api import get_lockers, BASE_URL
+MOCK_RESPONSE = {
+    "items": [
+        {
+            "name": "PIS1",
+            "status": "Operating",
+            "location_247":"true",
+            "opening_hours": "24/7",
+            "easy_access_zone": "true",
+            "location_description": "Obok domu z różowym dachem",
+            "location_description_1": None,
+            "location_description_2": None,
+            "address": {
+                "line1": "ul. Kasztanowa 47b",
+                "line2": "32-064 Pisary"
+            },
+            "address_details": {
+                "city": "Pisary",
+                "post_code": "32-064"
+            }
+        }
+    ]
+}
+
+@patch("src.ilf.api.get_lockers")
+#when the function runs, find get_lockers and replace it with a dummy object
+def test_get_lockers(mock_get_lockers):
+    #pass the dummy object into the function as mock_get_lockers
+    mock_get_lockers.return_value.status_code = 200
+    #guarantee that the dummy object reports 200
+    mock_get_lockers.return_value.json.return_value = MOCK_RESPONSE
+    #When the code calls get_lockers dont connect to the internet, but
+    #just go with MOCK_RESPONSE
+    result = get_lockers("Pisary")
+    #act - call the real function and give it the MOCK_RESPONSE
+    assert len(result) == 1
+    assert result[0]["name"] == "PIS1"
+
+    mock_get_lockers.assert_called_once_with(
+        BASE_URL,
+        params = {
+            "city": "Pisary",
+        }
+    )
